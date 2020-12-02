8ecd768b798d4f70c50f23f46908ad7bcfc3d65e
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-12-01 22:46:58 -0300

    Criação de cardsfrontend

---
+++ frontend/app/src/cards/multiple_cards.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import Card from './cards';
+
+import img_turing_machine_I from '../assets/turing-machine-I.png';
+import img_turing_machine_II from '../assets/turing-machine-II.jpg';
+import img_turing_machine_III from '../assets/turing-machine-III.jpg';
+
+class multipleCards extends Component {
+    render() {
+        return(
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                    <div className="col-md-4">
+                        <Card 
+                            imgsrc={img_turing_machine_I} 
+                            title="Turing Machine I"
+                            description="This is a very beautiful turing machine.
+
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Mollitia perferendis aut deleniti pariatur asperiores 
+                            laboriosam voluptatum quidem modi enim minima dolores maxime, 
+                            eum quo minus nisi aliquid tempore iusto eaque?"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card 
+                            imgsrc={img_turing_machine_II} 
+                            title="Turing Machine II"
+                            description="This is a very beautiful turing machine.
+
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Mollitia perferendis aut deleniti pariatur asperiores 
+                            laboriosam voluptatum quidem modi enim minima dolores maxime, 
+                            eum quo minus nisi aliquid tempore iusto eaque?"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card 
+                            imgsrc={img_turing_machine_III} 
+                            title="Turing Machine III"
+                            description="This is a very beautiful turing machine.
+
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Mollitia perferendis aut deleniti pariatur asperiores 
+                            laboriosam voluptatum quidem modi enim minima dolores maxime, 
+                            eum quo minus nisi aliquid tempore iusto eaque?"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default multipleCards;
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import Card from './cards/cards';
+import Card from './cards/multiple_cards';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Card />,
   document.getElementById('root')
 );
 
