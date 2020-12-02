469959a88aed2332b5547ecef8b8ae26d3a44a3c
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-12-01 23:30:40 -0300

    Update multiple-cards

--- frontend/app/src/cards/multiple_cards.jsx
+++ frontend/app/src/cards/multiple_cards.jsx
@@ -8,7 +8,43 @@ import img_turing_machine_III from '../assets/turing-machine-III.jpg';
 class multipleCards extends Component {
     render() {
         return(
-            <div className="container-fluid d-flex justify-content-center">
+            <div className="container justify-content-center">
+                <div className="row">
+                    <div className="col-md-4">
+                        <Card 
+                            imgsrc={img_turing_machine_I} 
+                            title="Turing Machine I"
+                            description="This is a very beautiful turing machine.
+
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Mollitia perferendis aut deleniti pariatur asperiores 
+                            laboriosam voluptatum quidem modi enim minima dolores maxime, 
+                            eum quo minus nisi aliquid tempore iusto eaque?"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card 
+                            imgsrc={img_turing_machine_II} 
+                            title="Turing Machine II"
+                            description="This is a very beautiful turing machine.
+
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Mollitia perferendis aut deleniti pariatur asperiores 
+                            laboriosam voluptatum quidem modi enim minima dolores maxime, 
+                            eum quo minus nisi aliquid tempore iusto eaque?"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card 
+                            imgsrc={img_turing_machine_III} 
+                            title="Turing Machine III"
+                            description="This is a very beautiful turing machine.
+
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Mollitia perferendis aut deleniti pariatur asperiores 
+                            laboriosam voluptatum quidem modi enim minima dolores maxime, 
+                            eum quo minus nisi aliquid tempore iusto eaque?"/>
+                    </div>
+                </div>
+                
                 <div className="row">
                     <div className="col-md-4">
                         <Card 
