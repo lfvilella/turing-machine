4d5374912b11161e0c61d35f8aec57c69918c1a7
Luis Felipe Vilella <45940140+lfvilella@users.noreply.github.com>
2020-12-09 13:40:45 -0300

    Integrating with axios (#10)
    * add hooks on cards

--- frontend/app/src/cards/cards.jsx
+++ frontend/app/src/cards/cards.jsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './cards_style.css';
 
+const axios = require('axios');
+axios.defaults.baseURL = 'http://localhost:8000/api/v.1';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 const Card = props => {
-    return(
-        <div className="card text-center shadow">
-            <div className="overflow">
-                <img src={props.imgsrc} alt="Image turing machine I" className="card-img-top"/>
-            </div>
-            <div className="card-body text-dark">
-                <h4 className="card-title">{props.title}</h4> 
-                <p className="card-text text-secondary">
-                    {props.description}
-                </p> 
-                <a href="#" className="btn btn-outline-success">Go turing machine</a> 
-            </div>
+  const [tapeData, setTapeData] = useState('');
+  const [machineResult, setMachinerResult] = useState({});
+
+  function sendTape() {
+    return axios.post(
+      `/machine/${props.pathUrl}`, { "tape": tapeData }
+    ).then((response) => {
+      setMachinerResult(response.data)
+      // console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  const renderResult = () => {
+    return (
+      <div className='output'>
+        <h6>Saida: <b>{machineResult.output}</b></h6>
+        <p>{JSON.stringify(machineResult)}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="card text-center shadow">
+      <div className="overflow">
+        <img src={props.imgsrc} alt="Image turing machine I" className="card-img-top" />
+      </div>
+      <div className="card-body text-dark">
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text text-secondary">
+          {props.description}
+        </p>
+        <hr />
+        <div>
+          <h6>Insira na fita abaixo para testar a máquina!</h6>
+          {/* <input type="text" name="" id="" placeholder={props.placeholder} /> */}
+          <input type="text" name="tapeData" value={tapeData} onChange={e => setTapeData(e.target.value)} placeholder={props.placeholder} />
+          <br /><br />
+          <button onClick={sendTape} className="btn btn-outline-success">Run Machine</button>
         </div>
-    );
+        {renderResult()}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
